c9a058b968776cab40217bb42f261077914726c8
kazkosa <kazu_kosa@icloud.com>
2019-09-11 12:37:41 +0900

    create message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,88 @@
+$(function(){
+  
+  function appendMsgToHTML(message){
+    var html;
+    if(message.image_url){
+      //画像投稿ありのケース
+      html = `
+      <div class="message">
+        <li class="right-contents__messages__list__message">
+          <div class="right-contents__messages__list__message__usertime">
+            <p class="right-contents__messages__list__message__usertime_user">
+              ${message.user_name}
+            </p>
+            <p class="right-contents__messages__list__message__usertime_time">
+              ${message.created_at}
+            </p>
+          </div>
+          <p class="right-contents__messages__list__message__text">
+            ${message.content}
+          </p>
+          <img src=${message.image_url} class="lower-message__image"></img>
+        </li>
+      </div>
+      `;
+    }else{
+      //画像投稿なしかつテキスト投稿ありのケース
+      html = `
+      <div class="message">
+        <li class="right-contents__messages__list__message">
+          <div class="right-contents__messages__list__message__usertime">
+            <p class="right-contents__messages__list__message__usertime_user">
+              ${message.user_name}
+            </p>
+            <p class="right-contents__messages__list__message__usertime_time">
+              ${message.created_at}
+            </p>
+          </div>
+          <p class="right-contents__messages__list__message__text">
+            ${message.content}
+          </p>
+
+        </li>
+      </div>
+      `;
+    }
+    $(".right-contents__messages__list").append(html);
+  }
+  //Send btnによる発火処理
+  $("#new_message").on("submit",function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var message = $("#message_content").val();
+    var url =$(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      if(message.created_at){
+        //validete結果passした場合の処理
+
+        appendMsgToHTML(message);
+
+        var target = $(".right-contents__messages__list__message").last();
+        var position = target.offset().top + $(".right-contents__messages").scrollTop();
+        $(".right-contents__messages").animate({scrollTop: position}, 300, 'swing');
+        
+
+        $("#message_content").val("");
+        $("input[type='file']").val("");
+        $(".new__message__submit-btn").removeAttr("disabled");
+
+      }else{
+        //validete結果failした場合の処理
+        alert("メッセージを入力してください");
+        $(".new__message__submit-btn").removeAttr("disabled");
+      }
+    })
+    //500or404error時
+    .fail(function(){
+      alert("Error");
+    })
+  });
+});
